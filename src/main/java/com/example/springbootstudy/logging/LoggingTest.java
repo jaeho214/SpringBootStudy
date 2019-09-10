2dd69fa20d57b5e8501160652f86c081135c3157
jaeho214 <jaeho214@naver.com>
2019-09-11 00:42:43 +0900

    Spring Boot study

---
+++ src/main/java/com/example/springbootstudy/logging/LoggingTest.java
@@ -0,0 +1,9 @@
+package com.example.springbootstudy.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+}
---
+++ src/main/java/com/example/springbootstudy/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.example.springbootstudy.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "jaeho";
+    }
+}
